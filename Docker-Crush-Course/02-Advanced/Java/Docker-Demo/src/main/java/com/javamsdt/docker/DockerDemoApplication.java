7efd10dbef5dc54348a0ebede59b4eda237a0600
Ahmed Samy <serenitydiver@hotmail.com>
2025-09-11 16:11:24 +0200

    Cleaning up

--- Docker-Crush-Course/02-Advanced/Java/Docker-Demo/src/main/java/com/javamsdt/docker/DockerDemoApplication.java
+++ Docker-Crush-Course/02-Advanced/Java/Docker-Demo/src/main/java/com/javamsdt/docker/DockerDemoApplication.java
@@ -1,9 +1,5 @@
 package com.javamsdt.docker;
 
-import com.javamsdt.docker.modal.User;
-import com.javamsdt.docker.repository.UserRepository;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
